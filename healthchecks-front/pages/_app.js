16deade2589f0c27582ae338c9a3cf9f382dd064
Victor Zheng <vzheng@gmail.com>
2022-02-27 22:03:18 -0800

    instructions

--- healthchecks-front/pages/_app.js
+++ healthchecks-front/pages/_app.js
@@ -1,7 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
-
+import Script from "next/script";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
