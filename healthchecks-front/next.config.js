13a4879c13391cab70650f381d72020d28de2a64
Jimmy Yang <jimmyy1337@gmail.com>
2022-03-16 18:37:41 -0400

    Target repo name

--- healthchecks-front/next.config.js
+++ healthchecks-front/next.config.js
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  assetPrefix: isProd ? '/WATonomous/status/' : '',
+  assetPrefix: '/status/',
   reactStrictMode: true,
 }
