16cfb55d2a32d4d11465c30b68e27b81fc2befac
Ben Zhang <benzhangniu@gmail.com>
2022-03-26 01:05:09 -0400

    Use relative path for _next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   reactStrictMode: true,
+  // Partially resolves the relative path issue: https://github.com/vercel/next.js/issues/2581#issuecomment-451710217
+  assetPrefix: './',
 };
