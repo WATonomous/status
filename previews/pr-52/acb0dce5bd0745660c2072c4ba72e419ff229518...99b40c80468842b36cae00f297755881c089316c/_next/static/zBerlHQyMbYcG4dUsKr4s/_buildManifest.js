4bf1fb1b7708e12a07006673032cda33176b67a1
jimmyygit <jimmyygit@users.noreply.github.com>
2022-04-03 00:25:15 +0000

    Deploying to gh-pages from @ WATonomous/status@33c4d5c532ba3b0d88a875bbd4689c1133bd2958 🚀

---
+++ previews/pr-52/acb0dce5bd0745660c2072c4ba72e419ff229518...99b40c80468842b36cae00f297755881c089316c/_next/static/zBerlHQyMbYcG4dUsKr4s/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-c2ffd8d428a8879e.js","static/chunks/215-89e0851661a18145.js","static/chunks/pages/index-3ac9663749b56ed2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
