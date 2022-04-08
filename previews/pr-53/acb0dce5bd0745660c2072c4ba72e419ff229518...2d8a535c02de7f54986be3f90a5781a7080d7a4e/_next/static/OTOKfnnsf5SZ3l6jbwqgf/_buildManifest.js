30f884569fbba08946a2e805dc5847bcf46ff7a9
ben-z <ben-z@users.noreply.github.com>
2022-04-08 03:54:41 +0000

    Deploying to gh-pages from @ WATonomous/status@0292845541f518d412b680f882c8a5246080e025 🚀

---
+++ previews/pr-53/acb0dce5bd0745660c2072c4ba72e419ff229518...2d8a535c02de7f54986be3f90a5781a7080d7a4e/_next/static/OTOKfnnsf5SZ3l6jbwqgf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-39ba45af86341a9b.js","static/chunks/6526-d445e201e4811595.js","static/chunks/pages/index-3b44880c43c632fd.js"],"/_error":["static/chunks/pages/_error-25839e52160ad85d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
