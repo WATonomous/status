094a88c4f685d92ff8d2e9a43c13fef3798bd1a9
JodyZ0203 <JodyZ0203@users.noreply.github.com>
2023-02-08 04:51:48 +0000

    Deploying to gh-pages from @ WATonomous/status@f18726252ea3059d181a167046202bedf361afb5 🚀

---
+++ previews/pr-61/7b15c11a5a7d0847dc7db56275469fe9cf82de25...f0467a23da69dc2ead918333c0fcaf079a58255d/_next/static/r7465nZqAckyNcXPF78Qp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-e61c2e0d9c9a0957.js","static/chunks/834-81bea80265257f9a.js","static/chunks/pages/index-5b3d260b515bd08f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
