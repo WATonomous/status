2fa2534a87269de98eca8ff19c58e14a2172ad52
Ben Zhang <ben.zhang@watonomous.ca>
2024-07-15 06:55:01 +0000

    Tune comment

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,10 @@ export const HEALTHCHECKSIO_API_KEY = 'tCsst0GSKpfvslmpmlsmivRrUCRuv6Iv'
 // read-only API key with the "org scope" for Sentry. Obtained by creating an internal integration:
 // https://watonomous.sentry.io/settings/developer-settings/statuswatonomousca-be7323/
 // 
-// "Authorized JavaScript Origins" is set to `localhost` and `status.watonomous.ca`.
+// "Authorized JavaScript Origins" is set to:
+// - localhost
+// - status.watonomous.ca
+// - developer-specific domains like abcd.use.devtunnels.ms
 // We'd get a 400 error with the message "Invalid origin" otherwise.
 // https://github.com/getsentry/sentry/issues/15310#issuecomment-650443556
 export const SENTRY_API_KEY = '53ab13b4337a937bc66eec3a4b3f836b14dd51890fde15fd51b9523eb598382c'
