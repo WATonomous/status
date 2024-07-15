bd7a7dc59d30f7ba921ae21859a3ed15c3cf7fe3
Ben Zhang <ben.zhang@watonomous.ca>
2024-07-15 23:03:18 +0000

    Integrate source map upload to the build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.DEPLOY_PATH || '/',
-  plugins: [react()],
+
+  plugins: [
+    react(),
+    // Manually generated an auth token with project:releases scope and
+    // added the SENTRY_AUTH_TOKEN env var to CI.
+    // https://watonomous.sentry.io/settings/developer-settings/status-page-source-map-upload-db69ba/
+    sentryVitePlugin({
+      org: "watonomous",
+      project: "status-page",
+      telemetry: false,
+    })
+  ],
+
+  build: {
+    sourcemap: true
+  }
 })
