1464c3f602f86ecbbcd7de657742acac8121a1de
Vincent Lin <vincentln520@gmail.com>
2025-10-30 23:53:19 -0400

    Fix Healthchecks API calls

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // read-only API key for the healthchecks.io project. Obtained from 
 // https://healthchecks.io/projects/7acc2ad3-f672-40c9-a061-12c8fd128f8b/settings/
-export const HEALTHCHECKSIO_API_KEY = 'tCsst0GSKpfvslmpmlsmivRrUCRuv6Iv'
+export const HEALTHCHECKSIO_API_KEY = 'hcr_RNvMq5zwrGcvXurloN5mF9ino3qs'
 
 // read-only API key with the "org scope" for Sentry. Obtained by creating an internal integration:
 // https://watonomous.sentry.io/settings/developer-settings/statuswatonomousca-be7323/
